c7407b23633eeb50cbc2482c348a6742844d7289
Lucas Hinkle <lwhinkle@memphis.edu>
2024-04-23 17:31:45 -0500

    Small fix

--- weather.rb
+++ weather.rb
@@ -1,9 +1,9 @@
 require 'httparty'
 
-url = HTTParty.get('https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=0rfgdumRwpZCx2UQkdbBmckdJvZO1HuE')
+response = HTTParty.get('https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=0rfgdumRwpZCx2UQkdbBmckdJvZO1HuE')
 
-response = JSON.parse(url.body)
+parsed_response = JSON.parse(response.body)
 
-data = response.values[0].values[1]
+data = parsed_response.values[0].values[1]
 
 puts data
