569cf48c3bd49fea7da37ebc10f50256abfd3728
Lucas Hinkle <lwhinkle@memphis.edu>
2024-04-23 17:34:36 -0500

    Changes to currency

--- currency.rb
+++ currency.rb
@@ -1,8 +1,6 @@
 require 'httparty'
 require 'json'
 
-url = HTTParty.get('https://v6.exchangerate-api.com/v6/8449be411d2e51d07b97ccfc/latest/USD')
+response = HTTParty.get('https://v6.exchangerate-api.com/v6/8449be411d2e51d07b97ccfc/latest/USD')
 
-response = JSON.parse(url.body)
-
-puts response
+puts (response['conversion_rates']['EUR'] * 50)
